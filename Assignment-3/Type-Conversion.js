46a064163e99b4f638b2437e927469c28e373fa5
Akash Singh <akashsinghthakur60@gmail.com>
2023-07-19 11:48:27 +0530

    Type conversion

---
+++ Assignment-3/Type-Conversion.js
@@ -0,0 +1,19 @@
+// Implicit Conversion - automatic type conversion
+
+let result;
+
+result = '3' + 2; 
+console.log(result)
+
+let result1;
+
+result1 = '3' + true; 
+console.log(result1)
+
+// Explicit Conversion - manual type conversion
+
+let result2;
+
+// string to number
+result2 = Number('324');
+console.log(result2);
